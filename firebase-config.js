fe8418331b71aae4f1617ae1f7c06af3883de10c
LzLabs <luizcanalli@unochapeco.edu.br>
2026-08-18 19:42:24 -0300

    Conecta app ao projeto Firebase

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Configuração pública do Firebase Web SDK.
+// Não contém senha nem credencial administrativa; a autorização real é feita por Authentication + Firestore Rules.
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrvDaJZuiyN-bM1L4qw8lN8q-gQtjXbig",
+  authDomain: "meupatrimonio-4c878.firebaseapp.com",
+  projectId: "meupatrimonio-4c878",
+  storageBucket: "meupatrimonio-4c878.firebasestorage.app",
+  messagingSenderId: "457912347612",
+  appId: "1:457912347612:web:6413403bed3dd52809bd19"
+};
+
+// App Check será ativado após o primeiro teste funcional do domínio publicado.
+export const appCheckSiteKey = "";
