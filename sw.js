a07ccd2ff29fcbe34f270a85028df399ba1be01a
LzLabs <luizcanalli@unochapeco.edu.br>
2026-08-18 19:42:40 -0300

    Adiciona cache PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='meu-patrimonio-v1';
+const FILES=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));});
