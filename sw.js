7693018d25cbb0c62fcded38911e79166b9ed3ce
LzLabs <luizcanalli@unochapeco.edu.br>
2026-08-18 21:39:53 -0300

    Atualiza cache para indicadores financeiros revisados

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='meu-patrimonio-v4';
-const FILES=['./','./index.html','./styles.css','./app.js','./enhancements.js','./firebase-config.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='meu-patrimonio-v7';
+const FILES=['./','./index.html','./styles.css','./app.js','./enhancements.js','./integrity.js','./firebase-config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
