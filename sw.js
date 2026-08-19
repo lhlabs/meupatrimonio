3b012bac01340fd433fcd7387a170bb008a22b4e
LzLabs <luizcanalli@unochapeco.edu.br>
2026-08-18 21:53:39 -0300

    Atualiza cache após correção de travamento

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-patrimonio-v8';
+const CACHE='meu-patrimonio-v9';
 const FILES=['./','./index.html','./styles.css','./app.js','./enhancements.js','./integrity.js','./finance-v3.js','./firebase-config.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
