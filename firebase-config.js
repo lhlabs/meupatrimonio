a058f1a11e2123bc8582da82283cb8cde9a0d312
LzLabs <luizcanalli@unochapeco.edu.br>
2026-08-18 21:39:46 -0300

    Carrega camada de integridade financeira

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
 import "./enhancements.js";
+import "./integrity.js";
 
 // Configuração pública do Firebase Web SDK.
 // Não contém senha nem credencial administrativa; a autorização real é feita por Authentication + Firestore Rules.
